bc29e5e1213b03ff265be10e16ad314c2002754b
Oliver Ernst Nowak <onowak_boats@hotmail.com>
2011-08-14 18:24:26 -0400

    + added first pass at SVG-based home page

---
+++ scripts/main_script.js
@@ -0,0 +1,25 @@
+function handleImageClick()
+{
+	alert("IMAGE clicked");
+}
+
+function handleTextClick()
+{
+	alert("TEXT clicked");
+}
+
+function handleVideoClick()
+{
+	alert("VIDEO clicked");
+}
+	function handleAboutClick()
+{
+	alert("ABOUT click");
+}
+
+
+$(document).ready(function() 
+{
+	
+
+});
