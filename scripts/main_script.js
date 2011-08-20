6d43f08b98a4e25be3a1dd93382dd4eef0d1bd86
Oliver Ernst Nowak <onowak_boats@hotmail.com>
2011-08-20 14:37:16 -0400

    + re-organized scripts

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,25 +1,42 @@
-function handleImageClick()
-{
-	alert("IMAGE clicked");
-}
-
-function handleTextClick()
-{
-	alert("TEXT clicked");
-}
-
-function handleVideoClick()
-{
-	alert("VIDEO clicked");
-}
-	function handleAboutClick()
-{
-	alert("ABOUT click");
-}
-
-
 $(document).ready(function() 
 {
+
+	function handleImageClick()
+	{
+		alert("IMAGE clicked");
+	}
+
+	function handleTextClick()
+	{
+		alert("TEXT clicked");
+	}
+	
+	function handleVideoClick()
+	{
+		alert("VIDEO clicked");
+	}
+		
+	function handleAboutClick()
+	{
+		alert("ABOUT click");
+	}
+
+
+	$('#_image').click(handleImageClick);
+	$('#_image').hover(function() {$(this).addClass('mouseOver')}, 
+					   function() {$(this).removeClass('mouseOver')});
+					   
+	$('#_video').click(handleVideoClick);
+	$('#_video').hover(function() {$(this).addClass('mouseOver')}, 
+					   function() {$(this).removeClass('mouseOver')});
+					   
+	$('#_text').click(handleTextClick);
+	$('#_text').hover(function() {$(this).addClass('mouseOver')}, 
+					   function() {$(this).removeClass('mouseOver')});
+					   
+	$('#_about').click(handleAboutClick);
+	$('#_about').hover(function() {$(this).addClass('mouseOver')}, 
+					   function() {$(this).removeClass('mouseOver')});
 	
 
 });
